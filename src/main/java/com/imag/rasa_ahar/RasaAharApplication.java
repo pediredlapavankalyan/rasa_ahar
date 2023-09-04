75e3f9982bbb859f5fbe7fc2423d62bc95d0aff6
pediredlapavankalyan <pediredlapavankalyan@gmail.com>
2023-09-04 15:03:18 +0530

    04/sep

---
+++ src/main/java/com/imag/rasa_ahar/RasaAharApplication.java
@@ -0,0 +1,13 @@
+package com.imag.rasa_ahar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasaAharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasaAharApplication.class, args);
+	}
+
+}
